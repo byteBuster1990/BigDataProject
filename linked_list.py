101660edcb0e892fd3353ae6badff4c9de204e1d
Vin0990 <Vndraaghav90@gmail.com>
2024-01-21 12:30:23 -0500

    popfirst added

--- linked_list.py
+++ linked_list.py
@@ -24,15 +24,46 @@ class Linked_list:
             self.tail = new_node
         self.length += 1
     def pop(self):
+        if self.length == 0:
+            return None
         temp = self.head
         pre = self.head
+        
         while temp.next is not None:
-            temp = temp.next
             pre = temp
-            print(temp.value, pre.value)
-            
-
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None         
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None        
+        return temp
+    def prepend(self, value):
+        new_node = Node(value)
+        old_node = self.head
+        if self.head is not None:
+            self.head = new_node
+            self.head.next = old_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
+          
+    def popfirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp.value
+    
 
 my_linkedlist = Linked_list(21)
 my_linkedlist.append(43)
-my_linkedlist.pop()
+print(my_linkedlist.pop())
+my_linkedlist.prepend(23)
+print(my_linkedlist.popfirst())
